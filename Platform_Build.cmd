c0ed9055245c1099be517af2576872bcbe81376f
Iain Ballard <iain.ballard@7digital.com>
2012-04-26 16:25:19 +0100

    restructure again

--- Platform_Build.cmd
+++ Platform_Build.cmd
@@ -2,8 +2,8 @@
 cd %~dp0
 
 @echo off
-powershell -NoProfile -ExecutionPolicy ByPass ".\.build\SelfUpdate.ps1"
-powershell -NoProfile -ExecutionPolicy ByPass ".\.build\UpdateAll.ps1"
-powershell -NoProfile -ExecutionPolicy ByPass ".\.build\BuildAllNoUpdate.ps1"
+powershell -NoProfile -ExecutionPolicy ByPass ".\build\SelfUpdate.ps1"
+powershell -NoProfile -ExecutionPolicy ByPass ".\build\UpdateAll.ps1"
+powershell -NoProfile -ExecutionPolicy ByPass ".\build\BuildAllNoUpdate.ps1"
 @echo on 
 pause
--- Setup.cmd
+++ Setup.cmd
@@ -3,6 +3,6 @@ cd %~dp0
 rem Go install Powershell if it's not there:
 PKGMGR.EXE /iu:MicrosoftWindowsPowerShell
 
-powershell.exe -NoProfile -ExecutionPolicy ByPass ".\.build\InitialSetup.ps1"
+powershell.exe -NoProfile -ExecutionPolicy ByPass ".\build\InitialSetup.ps1"
 
 pause
